88fbb99fe85ec52c66725746f5dc0821ca32be1c
rigazilla <vittorio@infinispan.org>
2026-03-31 15:00:09 +0200

    removed nodejs

--- inmemory-catalogue-nodejs/create-proto.sh
+++
@@ -1,5 +0,0 @@
-curl --digest -u admin:secret \
-   -X POST \
-   -H "Content-Type: text/plain" \
-   --data-binary @retail-schema.proto \
-   http://localhost:11222/rest/v2/caches/___protobuf_metadata/retail-schema.proto
